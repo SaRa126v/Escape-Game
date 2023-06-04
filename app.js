9cab13aa522e1d89fb4468607793e3bd7bc72a79
SaRa <119528406+SaRa126v@users.noreply.github.com>
2023-06-04 13:43:13 +0430

    update
    egg speed with limitation

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-// egg movement with keyboard
+// egg movement 
 
 let egg = document.querySelector("#egg");
 let pace = 10;
 
+// img loading before script
 window.addEventListener('load', (e) =>{
  egg.style.position = 'absolute';
- egg.style.top = 5 ;
- egg.style.left = 5 ;
+ egg.style.top = 0 ;
+ egg.style.left = 0 ;
  })
 
+// move with keyboard
 window.addEventListener('keydown', (e) =>{
 switch (e.key) {
  case 'ArrowDown':
  egg.style.top = parseInt(egg.style.top) + pace + 'px';
+ console.log(egg.style.top);
+ console.log(pace);
   break;
   case 'ArrowUp':
    egg.style.top = parseInt(egg.style.top) - pace + 'px';
@@ -26,4 +30,19 @@ switch (e.key) {
 }
 })
 
+// speed up with limitation
+document.querySelector('#addBtn').addEventListener('click', ()=> {
+   if (pace < 100 ) {
+      pace = pace + 10;     
+   }
+  
+})
+
+// speed down with limitation
+document.querySelector('#substractBtn').addEventListener('click', ()=> {
+   if (pace > 10 ) {
+      pace = pace - 10;    
+   }
+   
+})
 
