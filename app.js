449bc4c12f6f84786edb889e7009ec603f9974a2
navidmsh <124199694+navidmsh@users.noreply.github.com>
2023-06-06 00:53:03 +0330

    2nd push - navid Egg-Escape
    مواردی که باید رفع و اضافه گردد:
    1- جان باقیمانده در حالت نهایی روی منفی 1 نمایش داده می شود
    2- اضافه کردن دکمه دوباره بعد از اتمام بازی توسط امیرحسین
    3- جلوگیری از خروج تخم مرغ از محدوده تعیین شده

--- app.js
+++ app.js
@@ -1,44 +1,43 @@
 // egg movement 
-
 let egg = document.querySelector("#egg");
 let pace = 10;
 
-// img loading before script
+// image loading before javascript 
 window.addEventListener('load', (e) =>{
- egg.style.position = 'absolute';
- egg.style.top = 0 ;
- egg.style.left = 0 ;
- })
+   egg.style.position = 'absolute';
+   egg.style.top = 0 ;
+   egg.style.left = 0 ;
+})
 
-// move with keyboard
+// move with arrows on the keyboard
 window.addEventListener('keydown', (e) =>{
 switch (e.key) {
- case 'ArrowDown':
- egg.style.top = parseInt(egg.style.top) + pace + 'px';
- console.log(egg.style.top);
+   case 'ArrowDown':
+      egg.style.top = parseInt(egg.style.top) + pace + 'px';
+      console.log(egg.style.top);
  console.log(pace);
   break;
   case 'ArrowUp':
-   egg.style.top = parseInt(egg.style.top) - pace + 'px';
-  break;
-  case 'ArrowLeft':
-   egg.style.left = parseInt(egg.style.left) - pace + 'px';
-  break;
+     egg.style.top = parseInt(egg.style.top) - pace + 'px';
+     break;
+     case 'ArrowLeft':
+        egg.style.left = parseInt(egg.style.left) - pace + 'px';
+        break;
   case 'ArrowRight':
-   egg.style.left = parseInt(egg.style.left) + pace + 'px';
-   break;
+     egg.style.left = parseInt(egg.style.left) + pace + 'px';
+     break;
 }
 })
 
-// speed up with limitation
+// speed up with '+' button
 document.querySelector('#addBtn').addEventListener('click', ()=> {
    if (pace < 100 ) {
       pace += 10;     
    }
-  
+   
 })
 
-// speed down with limitation
+// speed down with '-' button
 document.querySelector('#substractBtn').addEventListener('click', ()=> {
    if (pace > 10 ) {
       pace = pace - 10;    
@@ -52,28 +51,35 @@ document.addEventListener('keydown', (e)=> {
    e.preventDefault();
    // upercase to lowercase if any
    if (e.key.toLowerCase() === "x") {
-
+      
       if (pace < 100 ) {
-         pace += 10;           
+         pace += 10;         
       }    
       
    }
 })
 
-//*Decreas In The Number Of Heart and Message
+//*Decrease The value Of Hearts and display
+const livesValue = document.querySelector('#number');
+const gameover = document.querySelector('#gameover');
 
-let number =5;
-window.addEventListener("onclick",Decrea);
-function Decrea(){
-   if(number){
-      let decrea =
-      document.getElementById("number");
-      number--;
-      decrea.innerHTML =number;
-   }else if(number ==0){
-      document.getElementById("messagebox").style.display ="block";
-   }else if(number ==0){
-      
-      document.getElementById("resets").reset();
+let lives = 4;
+egg.addEventListener('click',function(){
+   lives--;
+      livesValue.textContent = lives;
+   if(lives ==0){
+      gameover.style.display = 'flex';
+      document.getElementById('heart5').style.display = "block";
+      // document.getElementById("messagebox").style.display ="flex";
+      // document.getElementById("resets").reset();
+      document.body.style.overflow = 'hidden';
+   }else if(lives === 1){
+      document.getElementById('heart4').style.display = "block";
+   }else if(lives === 2){
+      document.getElementById('heart3').style.display = "block";
+   }else if(lives === 3){
+      document.getElementById('heart2').style.display = "block";
+   }else{
+      lives = 0;
    }
-}
+});
