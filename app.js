c8cfe0e179e33c40c403e21f0741eca4a8a9a32c
SaRa <119528406+SaRa126v@users.noreply.github.com>
2023-06-06 11:44:40 +0430

    update
    sound

--- app.js
+++ app.js
@@ -3,59 +3,59 @@ let egg = document.querySelector("#egg");
 let pace = 10;
 
 // image loading before javascript 
-window.addEventListener('load', (e) =>{
+window.addEventListener('load', (e) => {
    egg.style.position = 'absolute';
-   egg.style.top = 0 ;
-   egg.style.left = 0 ;
+   egg.style.top = 0;
+   egg.style.left = 0;
 })
 
 // move with arrows on the keyboard
-window.addEventListener('keydown', (e) =>{
-switch (e.key) {
-   case 'ArrowDown':
-      egg.style.top = parseInt(egg.style.top) + pace + 'px';
-      console.log(egg.style.top);
- console.log(pace);
-  break;
-  case 'ArrowUp':
-     egg.style.top = parseInt(egg.style.top) - pace + 'px';
-     break;
-     case 'ArrowLeft':
-        egg.style.left = parseInt(egg.style.left) - pace + 'px';
-        break;
-  case 'ArrowRight':
-     egg.style.left = parseInt(egg.style.left) + pace + 'px';
-     break;
-}
+window.addEventListener('keydown', (e) => {
+   switch (e.key) {
+      case 'ArrowDown':
+         egg.style.top = parseInt(egg.style.top) + pace + 'px';
+         console.log(egg.style.top);
+         console.log(pace);
+         break;
+      case 'ArrowUp':
+         egg.style.top = parseInt(egg.style.top) - pace + 'px';
+         break;
+      case 'ArrowLeft':
+         egg.style.left = parseInt(egg.style.left) - pace + 'px';
+         break;
+      case 'ArrowRight':
+         egg.style.left = parseInt(egg.style.left) + pace + 'px';
+         break;
+   }
 })
 
 // speed up with '+' button
-document.querySelector('#addBtn').addEventListener('click', ()=> {
-   if (pace < 100 ) {
-      pace += 10;     
+document.querySelector('#addBtn').addEventListener('click', () => {
+   if (pace < 100) {
+      pace += 10;
    }
-   
+
 })
 
 // speed down with '-' button
-document.querySelector('#substractBtn').addEventListener('click', ()=> {
-   if (pace > 10 ) {
-      pace = pace - 10;    
+document.querySelector('#substractBtn').addEventListener('click', () => {
+   if (pace > 10) {
+      pace = pace - 10;
    }
-   
+
 })
 
 // speeding up with shortcut 
-document.addEventListener('keydown', (e)=> {
+document.addEventListener('keydown', (e) => {
    // prevent default shortcut 
-   e.preventDefault();
+   e.preventDefault()
    // upercase to lowercase if any
    if (e.key.toLowerCase() === "x") {
-      
-      if (pace < 100 ) {
-         pace += 10;         
-      }    
-      
+
+      if (pace < 100) {
+         pace += 10;
+      }
+
    }
 })
 
@@ -64,21 +64,39 @@ const livesValue = document.querySelector('#number');
 const gameover = document.querySelector('#gameover');
 
 let lives = 5;
-egg.addEventListener('click',function(){
+egg.addEventListener('click', function () {
    lives--;
-      livesValue.textContent = lives;
-   if(lives ==1){
+   livesValue.textContent = lives;
+
+         // hurt sound
+         let hurt_sound = new Audio('assets/sounds/mixkit-failure-arcade-alert-notification-240.wav');
+
+   if (lives == 1) {
       document.getElementById('heart5').style.display = "block";
       document.body.style.overflow = 'hidden';
-   }else if(lives ==2){
+      hurt_sound.play();
+
+   } else if (lives == 2) {
       document.getElementById('heart4').style.display = "block";
-   }else if(lives == 3){
+      hurt_sound.play();
+
+   } else if (lives == 3) {
       document.getElementById('heart3').style.display = "block";
-   }else if(lives == 4){
+      hurt_sound.play();
+
+   } else if (lives == 4) {
       document.getElementById('heart2').style.display = "block";
-   }else{
+      hurt_sound.play();
+
+   } else {
       gameover.style.display = 'block';
-      lives =1;
+      lives = 1;
+
+      // gameOver sound
+      let gameOver_sound = new Audio('assets/sounds/mixkit-game-over-trombone-1940.wav');
+      gameOver_sound.play();
+
+      // reset
       document.getElementById("reset").reset();
    }
 });
